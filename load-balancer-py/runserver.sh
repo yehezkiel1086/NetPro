e2fef411070d363ea6bc52716b94182c2310d84c
yehezkiel1086 <tsukikagemaikaka1@gmail.com>
2026-08-29 12:42:20 +0700

    feat: add load-balancer-py

---
+++ load-balancer-py/runserver.sh
@@ -0,0 +1,15 @@
+#jalankan 5 async_server
+
+python3 async_server.py 3000 &
+python3 async_server.py 3001 &
+python3 async_server.py 3002 &
+python3 async_server.py 3003 &
+python3 async_server.py 3004
+
+#jalankan 5 server_process_pool
+
+# python3 server_process_pool_http.py 5000 &
+# python3 server_process_pool_http.py 5001 &
+# python3 server_process_pool_http.py 5002 &
+# python3 server_process_pool_http.py 5003 &
+# python3 server_process_pool_http.py 5004
